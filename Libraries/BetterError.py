fdec50f186612fd5655b9fef90a72d1352f08ed4
GithubDestroyer.py <101750090+iHategithub9000@users.noreply.github.com>
2022-05-30 06:22:43 +0200

    added code

---
+++ Libraries/BetterError.py
@@ -0,0 +1,20 @@
+from colorama import Fore, Style
+
+def CustomError(strtitle, strtext, fatal):
+  if fatal == False:
+    print(Fore.RED + f"{strtitle}: {strtext}")
+    print(Style.RESET_ALL)
+  elif fatal == True:
+    print(Fore.RED + Style.DIM + f"{strtitle}: {strtext}")
+    print(Style.RESET_ALL)
+    exit(9)
+  else:
+      print(Fore.RED + 'invalid Parameter for "fatal" (boolean) ')
+      print(Style.RESET_ALL)
+
+def CustomWarning(strtitle, strtext):
+  print(Fore.YELLOW + f"{strtitle}: {strtext}")
+  print(Style.RESET_ALL)
+def Help():
+  print("CustomError(title(str), text(str), fatal(bool)) make an error")
+  print("CustomWarning(title(str), text(str)) make a warning")
